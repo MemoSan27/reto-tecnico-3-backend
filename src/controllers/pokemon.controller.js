471b8e6cd196838a008be18640f09a8f9c886afd
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-05 12:12:19 -0700

    getpokemonsbypage added

--- src/controllers/pokemon.controller.js
+++ src/controllers/pokemon.controller.js
@@ -10,3 +10,17 @@ exports.getAllPokemons = async (req, res) => {
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
+
+exports.getPokemonsByPage = async (req, res) => {
+    try {
+      const { page, limit } = req.query;
+      const offset = (page - 1) * limit;
+      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+      const pokemons = data.results.map((pokemon) => pokemon.name).sort();
+      res.json(pokemons);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server Error' });
+    }
+  };
+
