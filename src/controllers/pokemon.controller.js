4f4eee33768284cf2e51f7a14e2bea38fce01239
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-05 12:18:20 -0700

    searchPokemon added

--- src/controllers/pokemon.controller.js
+++ src/controllers/pokemon.controller.js
@@ -1,26 +1,36 @@
 const axios = require('axios');
 
 exports.getAllPokemons = async (req, res) => {
-  try {
-    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
-    const pokemons = data.results.map((pokemon) => pokemon.name).sort();
+    try {
+        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000');
+        const pokemons = data.results.map((pokemon) => pokemon.name).sort();
     res.json(pokemons);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server Error' });
+    }
 };
 
 exports.getPokemonsByPage = async (req, res) => {
     try {
-      const { page, limit } = req.query;
-      const offset = (page - 1) * limit;
-      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-      const pokemons = data.results.map((pokemon) => pokemon.name).sort();
-      res.json(pokemons);
+        const { page, limit } = req.query;
+        const offset = (page - 1) * limit;
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+        const pokemons = data.results.map((pokemon) => pokemon.name).sort();
+        res.json(pokemons);
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Server Error' });
+        console.error(error);
+        res.status(500).json({ error: 'Server Error' });
     }
-  };
+};
 
+exports.searchPokemons = async (req, res) => {
+    try {
+        const { search } = req.query;
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`);
+    res.json([data.name]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error interno del Server Error' });
+    }
+};
