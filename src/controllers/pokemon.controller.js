281d6d2787081c82ee3a6f7d71e887154e3344ff
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-05 22:29:48 -0700

    pdf implemented

--- src/controllers/pokemon.controller.js
+++ src/controllers/pokemon.controller.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const PDFDocument = require('pdfkit');
+
 
 exports.getAllPokemons = async (req, res) => {
     try {
@@ -35,3 +37,47 @@ exports.searchPokemons = async (req, res) => {
         res.status(500).json({ error: 'Server Error' });
     }
 };
+
+exports.getPokemonDetails = async (req, res) => {
+      const { pokeId } = req.params;
+        // Implementa lógica para obtener detalles del pokemon por su ID
+      try {
+      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+      const pdfDoc = new PDFDocument();
+      const { name, id, sprites, moves } = data;
+      const movements = moves.map(move => move.move.name);
+      const { front_default, front_shiny } = sprites;
+      
+      pdfDoc.fontSize(18).text(`Pokemon Id: ${id}`);
+      pdfDoc.moveDown();
+      pdfDoc.fontSize(18).text(`Pokemon Name: ${name.toUpperCase()}`);
+      pdfDoc.moveDown();
+      pdfDoc.fontSize(18).text(`Movements:`);
+      pdfDoc.fontSize(18).text(`${movements}`);
+      pdfDoc.moveDown();
+      pdfDoc.text(`Pokemon Image:`);
+      
+      pdfDoc.image(await getImageBuffer(front_default), {
+        width: 250,
+      });
+      
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', `attachment; filename=${id}.pdf`);
+      pdfDoc.pipe(res);
+      pdfDoc.end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server Error' });
+    }
+  
+}  
+
+const getImageBuffer = async (imageUrl) => {
+    try {
+      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+      return Buffer.from(response.data, 'binary');
+    } catch (error) {
+      throw error;
+    }
+  };
+
--- src/routes/pokemon.router.js
+++ src/routes/pokemon.router.js
@@ -5,5 +5,6 @@ const pokeController = require('../controllers/pokemon.controller');
 router.get('/', pokeController.getAllPokemons);
 router.get('/paginated', pokeController.getPokemonsByPage);
 router.get('/search/:id', pokeController.searchPokemons);
+router.get('/search/:pokeId/detail', pokeController.getPokemonDetails);
 
 module.exports = router;
